b5ae10ed7f2d072f8e506884783aaaef1d82a08a
Bintang Satria Mulya <bintangsatriamulya88@gmail.com>
2024-09-24 20:27:31 +0700

    Update Error Splash Screen & Intro Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:wikusama_cafe/pages/spalshscreen.dart';
-
+import 'package:wikusama_cafe/pages/intropage.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -12,13 +11,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'WIKUSAMA Cafe',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Splashscreen(),
+      home: IntroPage(),
     );
   }
 }
+
 
---
+++ lib/pages/intropage.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: 100000,
+        height: 100000,
+        color: Color(hexColor('e0c2a7')),
+        child: const Column(
+          children: [
+          ],
+        ),
+        
+      ),
+    );
+  }
+}
+
+int hexColor(String color) {
+  //adding prefix
+  String newColor = '0xff' + color;
+  //removing # sign
+  newColor = newColor.replaceAll('#', '');
+  //converting it to the integer
+  int finalColor = int.parse(newColor);
+  return finalColor;
+}
